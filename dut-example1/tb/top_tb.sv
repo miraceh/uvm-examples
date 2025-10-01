f77bba6864d37620602309b1d828453860c657ab
kagura <mackerelsama@gmail.com>
2025-09-30 21:21:16 -0400

    [TB] virtual interface first try

--- dut-example1/tb/top_tb.sv
+++ dut-example1/tb/top_tb.sv
@@ -2,6 +2,7 @@
 `include "uvm_macros.svh"
 
 import uvm_pkg::*;
+`include "my_if.sv"
 `include "my_driver.sv"
 
 module top_tb;
@@ -13,12 +14,15 @@ reg rx_dv;
 wire[7:0] txd;
 wire tx_en;
 
+my_if input_if(clk, rst_n);
+my_if output_if(clk, rst_n);
+
 dut my_dut(.clk(clk),
            .rst_n(rst_n),
-           .rxd(rxd),
-           .rx_dv(rx_dv),
-           .txd(txd),
-           .tx_en(tx_en));
+           .rxd(input_if.data),
+           .rx_dv(input_if.valid),
+           .txd(output_if.data),
+           .tx_en(output_if.valid));
 
 initial begin
    run_test("my_driver");
@@ -35,6 +39,10 @@ initial begin
    rst_n = 1'b0;
    #1000;
    rst_n = 1'b1;
+end
+
+initial begin
+   uvm_config_db#(virtual my_if)::set(null, "uvm_test_top", "vif", input_if);
 end
 
 endmodule
