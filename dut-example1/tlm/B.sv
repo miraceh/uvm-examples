15a186747e6a1edbba2d1ffdd9701743b65596b1
kagura <mackerelsama@gmail.com>
2025-10-12 21:55:24 -0400

    [TLM] blocking_get first try

--- dut-example1/tlm/B.sv
+++ dut-example1/tlm/B.sv
@@ -3,35 +3,27 @@
 class B extends uvm_component;
    `uvm_component_utils(B)
 
-   uvm_blocking_put_export#(my_transaction) B_export;
-   uvm_blocking_put_imp#(my_transaction, B) B_imp;
+   uvm_blocking_get_port#(my_transaction) B_port;
    function new(string name, uvm_component parent);
       super.new(name, parent);
    endfunction
 
    extern function void build_phase(uvm_phase phase);
-   extern function void connect_phase(uvm_phase phase);
-   extern function void put(my_transaction tr);
    extern virtual  task main_phase(uvm_phase phase);
 endclass
 
 function void B::build_phase(uvm_phase phase);
    super.build_phase(phase);
-   B_export = new("B_export", this);
-   B_imp = new("B_imp", this);
-endfunction
-
-function void B::connect_phase(uvm_phase phase);
-   super.connect_phase(phase);
-   B_export.connect(B_imp);
-endfunction
-
-function void B::put(my_transaction tr);
-   `uvm_info("B", "receive a transaction", UVM_LOW) 
-   tr.print();
+   B_port = new("B_port", this);
 endfunction
 
 task B::main_phase(uvm_phase phase);
+   my_transaction tr;
+   while(1) begin
+      B_port.get(tr);
+      `uvm_info("B", "get a transaction", UVM_LOW) 
+      tr.print();
+   end
 endtask
 
 `endif
