c363a4b13fca3d57b7daea3dc1f5065165af33c9
kagura <mackerelsama@gmail.com>
2025-10-17 21:23:55 -0400

    [Phase] Brother excution order

--- dut-example1/tb/top_tb.sv
+++ dut-example1/tb/top_tb.sv
@@ -4,12 +4,7 @@
 import uvm_pkg::*;
 `include "my_if.sv"
 `include "my_transaction.sv"
-`include "my_sequencer.sv"
-`include "my_driver.sv"
-`include "my_monitor.sv"
-`include "my_agent.sv"
-`include "my_model.sv"
-`include "my_scoreboard.sv"
+`include "A.sv"
 `include "my_env.sv"
 `include "base_test.sv"
 `include "my_case0.sv"
