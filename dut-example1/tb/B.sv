ee765136c2126cd4b44fbafeae2adee51d34e609
kagura <mackerelsama@gmail.com>
2025-10-18 23:01:41 -0400

    [Domain] phase jump in multi domain

--- dut-example1/tb/B.sv
+++ dut-example1/tb/B.sv
@@ -2,11 +2,13 @@
 `define B__SV
 class B extends uvm_component;
    uvm_domain new_domain;
+   bit has_jumped;
    `uvm_component_utils(B)
 
    function new(string name, uvm_component parent);
       super.new(name, parent);
       new_domain = new("new_domain");
+      has_jumped = 0;
    endfunction
 
    virtual function void connect_phase(uvm_phase phase);
@@ -14,9 +16,7 @@ class B extends uvm_component;
    endfunction
 
    extern virtual  task reset_phase(uvm_phase phase);
-   extern virtual  task post_reset_phase(uvm_phase phase);
    extern virtual  task main_phase(uvm_phase phase);
-   extern virtual  task post_main_phase(uvm_phase phase);
 endclass
 
 task B::reset_phase(uvm_phase phase);
@@ -26,19 +26,14 @@ task B::reset_phase(uvm_phase phase);
    phase.drop_objection(this);
 endtask
 
-task B::post_reset_phase(uvm_phase phase);
-   `uvm_info("B", "enter into post reset phase", UVM_LOW)
-endtask
-
 task B::main_phase(uvm_phase phase);
    phase.raise_objection(this);
    `uvm_info("B", "enter into main phase", UVM_LOW)
    #500;
+   if(!has_jumped) begin
+      phase.jump(uvm_reset_phase::get());
+      has_jumped = 1'b1;
+   end
    phase.drop_objection(this);
 endtask
-
-task B::post_main_phase(uvm_phase phase);
-   `uvm_info("B", "enter into post main phase", UVM_LOW)
-endtask
-
 `endif
