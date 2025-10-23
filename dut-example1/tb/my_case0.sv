4260f024398369f872db3e67f10a6dd9d66d4cf9
kagura <mackerelsama@gmail.com>
2025-10-23 19:49:26 -0400

    [Seq] rand var in sequence try

--- dut-example1/tb/my_case0.sv
+++ dut-example1/tb/my_case0.sv
@@ -1,20 +1,8 @@
 `ifndef MY_CASE0__SV
 `define MY_CASE0__SV
 
-class crc_seq extends uvm_sequence#(my_transaction);
-   `uvm_object_utils(crc_seq)
-   function  new(string name= "crc_seq");
-      super.new(name);
-   endfunction 
-   
-   virtual task body();
-      my_transaction tr;
-      `uvm_do_with(tr, {tr.crc_err == 1;
-                        tr.dmac == 48'h980F;})
-   endtask
-endclass
-
 class long_seq extends uvm_sequence#(my_transaction);
+   rand bit[47:0] ldmac; 
    `uvm_object_utils(long_seq)
    function  new(string name= "long_seq");
       super.new(name);
@@ -24,24 +12,24 @@ class long_seq extends uvm_sequence#(my_transaction);
       my_transaction tr;
       `uvm_do_with(tr, {tr.crc_err == 0;
                         tr.pload.size() == 1500;
-                        tr.dmac == 48'hF675;})
+                        tr.dmac == ldmac;})
+      tr.print();
    endtask
 endclass
 
 class case0_sequence extends uvm_sequence #(my_transaction);
+   my_transaction m_trans;
 
    function  new(string name= "case0_sequence");
       super.new(name);
    endfunction 
    
    virtual task body();
-      crc_seq cseq;
       long_seq lseq;
       if(starting_phase != null) 
          starting_phase.raise_objection(this);
       repeat (10) begin
-         `uvm_do(cseq)
-         `uvm_do(lseq)
+         `uvm_do_with(lseq, {lseq.ldmac == 48'hFFFF;})
       end
       #100;
       if(starting_phase != null) 
@@ -50,31 +38,6 @@ class case0_sequence extends uvm_sequence #(my_transaction);
 
    `uvm_object_utils(case0_sequence)
 endclass
-
-//class case0_sequence extends uvm_sequence #(my_transaction);
-//   function  new(string name= "case0_sequence");
-//      super.new(name);
-//   endfunction 
-//   
-//   virtual task body();
-//      my_transaction tr;
-//      if(starting_phase != null) 
-//         starting_phase.raise_objection(this);
-//      repeat (10) begin
-//         `uvm_do_with(tr, {tr.crc_err == 1;
-//                           tr.dmac == 48'h980F;})
-//         `uvm_do_with(tr, {tr.crc_err == 0;
-//                           tr.pload.size() == 1500;
-//                           tr.dmac == 48'hF675;})
-//      end
-//      #100;
-//      if(starting_phase != null) 
-//         starting_phase.drop_objection(this);
-//   endtask
-//
-//   `uvm_object_utils(case0_sequence)
-//endclass
-
 
 class my_case0 extends base_test;
 
