a23d865db1612af23a240d75f225f6e6d72159da
kagura <mackerelsama@gmail.com>
2025-10-24 18:05:10 -0400

    [Seq] simple sync between two sequences

--- dut-example1/tb/my_case0.sv
+++ dut-example1/tb/my_case0.sv
@@ -1,25 +1,49 @@
 `ifndef MY_CASE0__SV
 `define MY_CASE0__SV
-class case0_sequence extends uvm_sequence #(my_transaction);
+event send_over;//global event
+class drv0_seq extends uvm_sequence #(my_transaction);
    my_transaction m_trans;
+   `uvm_object_utils(drv0_seq)
 
-   function  new(string name= "case0_sequence");
+   function  new(string name= "drv0_seq");
       super.new(name);
    endfunction 
    
    virtual task body();
       if(starting_phase != null) 
          starting_phase.raise_objection(this);
+      `uvm_do_with(m_trans, {m_trans.pload.size == 1500;})
+      ->send_over;
       repeat (10) begin
          `uvm_do(m_trans)
-         `uvm_info("sequence0", "send one transaction", UVM_MEDIUM)
+         `uvm_info("drv0_seq", "send one transaction", UVM_MEDIUM)
       end
       #100;
       if(starting_phase != null) 
          starting_phase.drop_objection(this);
    endtask
+endclass
 
-   `uvm_object_utils(case0_sequence)
+class drv1_seq extends uvm_sequence #(my_transaction);
+   my_transaction m_trans;
+   `uvm_object_utils(drv1_seq)
+
+   function  new(string name= "drv1_seq");
+      super.new(name);
+   endfunction 
+   
+   virtual task body();
+      if(starting_phase != null) 
+         starting_phase.raise_objection(this);
+      @send_over;
+      repeat (10) begin
+         `uvm_do(m_trans)
+         `uvm_info("drv1_seq", "send one transaction", UVM_MEDIUM)
+      end
+      #100;
+      if(starting_phase != null) 
+         starting_phase.drop_objection(this);
+   endtask
 endclass
 
 
@@ -39,11 +63,11 @@ function void my_case0::build_phase(uvm_phase phase);
    uvm_config_db#(uvm_object_wrapper)::set(this, 
                                            "env0.i_agt.sqr.main_phase", 
                                            "default_sequence", 
-                                           case0_sequence::type_id::get());
+                                           drv0_seq::type_id::get());
    uvm_config_db#(uvm_object_wrapper)::set(this, 
                                            "env1.i_agt.sqr.main_phase", 
                                            "default_sequence", 
-                                           case0_sequence::type_id::get());
+                                           drv1_seq::type_id::get());
 endfunction
 
 `endif
