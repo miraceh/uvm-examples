6fd821fb17ea71233b80495cfda56c135bc9ecf5
kagura <mackerelsama@gmail.com>
2025-11-21 21:17:46 -0500

    [Factory] sequence override

--- dut-example1/tb/my_case0.sv
+++ dut-example1/tb/my_case0.sv
@@ -18,26 +18,50 @@ class normal_sequence extends uvm_sequence #(my_transaction);
    endtask 
    
    virtual task body();
-      repeat (10) begin
-         `uvm_do(m_trans)
-         m_trans.print();
-      end
-      #100;
+      `uvm_do(m_trans)
+      m_trans.print();
    endtask
 
    `uvm_object_utils(normal_sequence)
 endclass
 
-class crc_err_tr extends my_transaction;
-   `uvm_object_utils(crc_err_tr)
-   function  new(string name= "crc_err_tr");
+class case_sequence extends uvm_sequence #(my_transaction);
+   `uvm_object_utils(case_sequence)
+   function  new(string name= "case_sequence");
+      super.new(name);
+   endfunction
+   
+   virtual task pre_body();
+      if(starting_phase != null) 
+         starting_phase.raise_objection(this);
+   endtask 
+   virtual task post_body();
+      if(starting_phase != null) 
+         starting_phase.drop_objection(this);
+   endtask 
+   
+   virtual task body();
+      normal_sequence nseq;
+      repeat(10) begin
+         `uvm_do(nseq)
+      end
+   endtask 
+endclass
+
+class abnormal_sequence extends normal_sequence;
+   `uvm_object_utils(abnormal_sequence)
+   function  new(string name= "abnormal_sequence");
       super.new(name);
    endfunction 
 
-   constraint crc_err_cons{
-      crc_err == 1;
-   }
+   virtual task body();
+      m_trans = new("m_trans");
+      m_trans.crc_err_cons.constraint_mode(0);
+      `uvm_rand_send_with(m_trans, {crc_err == 1;}) 
+      m_trans.print();
+   endtask
 endclass
+
 
 class my_case0 extends base_test;
 
@@ -52,11 +76,11 @@ endclass
 function void my_case0::build_phase(uvm_phase phase);
    super.build_phase(phase);
 
-   factory.set_type_override_by_type(my_transaction::get_type(), crc_err_tr::get_type());
+   factory.set_type_override_by_type(normal_sequence::get_type(), abnormal_sequence::get_type());
    uvm_config_db#(uvm_object_wrapper)::set(this, 
                                            "env.i_agt.sqr.main_phase", 
                                            "default_sequence", 
-                                           normal_sequence::type_id::get());
+                                           case_sequence::type_id::get());
 endfunction
 
 `endif
