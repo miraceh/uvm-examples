e0e81bf2a501b0d636fa67cea5f21468bf10008b
kagura <mackerelsama@gmail.com>
2025-11-21 20:41:51 -0500

    [Factory] common debug function

--- dut-example1/tb/my_case0.sv
+++ dut-example1/tb/my_case0.sv
@@ -21,46 +21,18 @@ class case0_sequence extends uvm_sequence #(my_transaction);
    `uvm_object_utils(case0_sequence)
 endclass
 
-class bird extends uvm_object;
-   virtual function void hungry();
-      $display("I am a bird, I am hungry");
-   endfunction
-   function void hungry2();
-      $display("I am a bird, I am hungry2");
+class new_monitor extends my_monitor;
+   `uvm_component_utils(new_monitor)
+   function new(string name = "new_monitor", uvm_component parent = null);
+      super.new(name, parent);
    endfunction
 
-   `uvm_object_utils(bird)
-   function new(string name = "bird");
-      super.new(name);
-   endfunction 
-endclass
-
-class parrot extends bird;
-   virtual function void hungry();
-      $display("I am a parrot, I am hungry");
-   endfunction
-   function void hungry2();
-      $display("I am a parrot, I am hungry2");
-   endfunction
-
-   `uvm_object_utils(parrot)
-   function new(string name = "parrot");
-      super.new(name);
-   endfunction 
-endclass
-
-class sparrow extends bird;
-   virtual function void hungry();
-      $display("I am a sparrow, I am hungry");
-   endfunction
-   function void hungry2();
-      $display("I am a sparrow, I am hungry2");
-   endfunction
-
-   `uvm_object_utils(sparrow)
-   function new(string name = "sparrow");
-      super.new(name);
-   endfunction 
+   virtual task main_phase(uvm_phase phase);
+      fork
+         super.main_phase(phase);
+      join_none
+      `uvm_info("new_monitor", "I am new monitor", UVM_MEDIUM)
+   endtask
 endclass
 
 class my_case0 extends base_test;
@@ -69,24 +41,28 @@ class my_case0 extends base_test;
       super.new(name,parent);
    endfunction 
    extern virtual function void build_phase(uvm_phase phase); 
-   extern virtual function void print_hungry(bird b_ptr); 
+   extern virtual function void connect_phase(uvm_phase phase); 
    `uvm_component_utils(my_case0)
 endclass
 
-function void my_case0::print_hungry(bird b_ptr);
-   b_ptr.hungry();
-   b_ptr.hungry2();
-endfunction
 
 function void my_case0::build_phase(uvm_phase phase);
-   bird bird_inst;
    super.build_phase(phase);
-   
-   set_type_override_by_type(bird::get_type(), parrot::get_type());
-   set_type_override_by_type(parrot::get_type(), sparrow::get_type(), 0);
-   
-   bird_inst = bird::type_id::create("bird_inst");
-   print_hungry(bird_inst);
+
+   set_inst_override_by_type("env.o_agt.mon", my_monitor::get_type(), new_monitor::get_type());
+   //set_inst_override("env.o_agt.mon", "my_monitor", "new_monitor");
+   uvm_config_db#(uvm_object_wrapper)::set(this, 
+                                           "env.i_agt.sqr.main_phase", 
+                                           "default_sequence", 
+                                           case0_sequence::type_id::get());
 endfunction
 
+function void my_case0::connect_phase(uvm_phase phase);
+   super.connect_phase(phase);
+   //env.o_agt.mon.print_override_info("my_monitor");
+   //factory.debug_create_by_type(my_monitor::get_type(), "uvm_test_top.env.o_agt.mon");
+   //factory.print(2);
+   //uvm_top.print_topology();
+   uvm_top.print();
+endfunction
 `endif
